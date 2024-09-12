0d489d2ee0ce17e97e35ddfb02f30d0c812dd532
angelguillermo1 <147960757+AngelGuillermo1@users.noreply.github.com>
2024-09-12 01:15:35 -0600

    Formulario Egreso

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import HomeScreen from './src/screens/homescreen'
+import { NavigationContainer } from "@react-navigation/native";
+import { View, StyleSheet, SafeAreaView } from "react-native";
+import Formulario from "./src/screens/FormularioEgreso";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  return(
+    <SafeAreaView style={styles.container}>
+        <Formulario/>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      padding: 16,
+      backgroundColor:'#c9f4eb'
+    },
+  });
---
+++ src/screens/FormularioEgreso.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import { View, TextInput, Button, StyleSheet, Text, Alert } from 'react-native';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  hipoteca: Yup.string().default(''),
+  Canasta: Yup.string().default(''),
+  Prestamos: Yup.string().default(''),
+  Transporte: Yup.string().default(''),
+  Servicios: Yup.string().default(''),
+  Seguro: Yup.string().default(''),
+  Varios: Yup.string().default(''),
+}).test(
+  'at-least-one-required',
+  'Al menos uno de los campos debe estar lleno',
+  values => values.hipoteca.trim() !== '' || 
+            values.Canasta.trim() !== '' || 
+            values.Prestamos.trim() !== '' || 
+            values.Transporte.trim() !== '' || 
+            values.Servicios.trim() !== '' || 
+            values.Seguro.trim() !== '' || 
+            values.Varios.trim() !== ''
+);
+
+const Formulario = () => {
+  const handleSubmit = (values) => {
+    Alert.alert('Formulario Enviado');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Formik
+        validationSchema={validationSchema}
+        initialValues={{ hipoteca: '', Canasta:'', Prestamos:'', Transporte:'', Servicios:'', Seguro:'', Varios:'' }}
+        onSubmit={handleSubmit}
+      >
+        {({ handleChange, handleBlur, handleSubmit, errors, touched, values }) => (
+          <>
+            <Text style={styles.texto}>• Alquiler/Hipoteca</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChange('hipoteca')}
+              onBlur={handleBlur('hipoteca')}
+              value={values.hipoteca}
+              placeholder="Digite el monto de la hipoteca"
+            />
+            {touched.hipoteca && errors.hipoteca && (
+              <Text style={styles.errorText}>{errors.hipoteca}</Text>
+            )}
+
+            <Text style={styles.texto}>• Canasta Básica</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChange('Canasta')}
+              onBlur={handleBlur('Canasta')}
+              value={values.Canasta}
+              placeholder="Cuanto gasta en víveres"
+            />
+            {touched.Canasta && errors.Canasta && (
+              <Text style={styles.errorText}>{errors.Canasta}</Text>
+            )}
+
+            <Text style={styles.texto}>• Prestamos</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChange('Prestamos')}
+              onBlur={handleBlur('Prestamos')}
+              value={values.Prestamos}
+              placeholder="Monto en prestamos"
+            />
+            {touched.Prestamos && errors.Prestamos && (
+              <Text style={styles.errorText}>{errors.Prestamos}</Text>
+            )}
+
+            <Text style={styles.texto}>• Transporte</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChange('Transporte')}
+              onBlur={handleBlur('Transporte')}
+              value={values.Transporte}
+              placeholder="Costo del transporte"
+            />
+            {touched.Transporte && errors.Transporte && (
+              <Text style={styles.errorText}>{errors.Transporte}</Text>
+            )}
+
+            <Text style={styles.texto}>• Servicios</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChange('Servicios')}
+              onBlur={handleBlur('Servicios')}
+              value={values.Servicios}
+              placeholder="Cuanto gasta en servicios"
+            />
+            {touched.Servicios && errors.Servicios && (
+              <Text style={styles.errorText}>{errors.Servicios}</Text>
+            )}
+
+            <Text style={styles.texto}>• Seguro</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChange('Seguro')}
+              onBlur={handleBlur('Seguro')}
+              value={values.Seguro}
+              placeholder="El costo de los seguros"
+            />
+            {touched.Seguro && errors.Seguro && (
+              <Text style={styles.errorText}>{errors.Seguro}</Text>
+            )}
+
+            <Text style={styles.texto}>• Gastos varios</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={handleChange('Varios')}
+              onBlur={handleBlur('Varios')}
+              value={values.Varios}
+              placeholder="Establecer si tiene gastos varios"
+            />
+            {touched.Varios && errors.Varios && (
+              <Text style={styles.errorText}>{errors.Varios}</Text>
+            )}
+
+            <Button
+              title="Enviar"
+              onPress={handleSubmit}
+              disabled={
+                !(
+                  values.hipoteca.trim() !== '' || 
+                  values.Canasta.trim() !== '' || 
+                  values.Prestamos.trim() !== '' || 
+                  values.Transporte.trim() !== '' || 
+                  values.Servicios.trim() !== '' || 
+                  values.Seguro.trim() !== '' || 
+                  values.Varios.trim() !== ''
+                )
+              }
+            />
+          </>
+        )}
+      </Formik>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 8,
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 8,
+  },
+  texto: {
+    fontSize: 16,
+    marginBottom: 8,
+  }
+});
+
+export default Formulario;
