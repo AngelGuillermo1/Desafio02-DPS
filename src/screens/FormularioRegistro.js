4dad5b8c607d55718ae4ba4708778457b68c07a4
Cartagena2000 <144555706+Cartagena2000@users.noreply.github.com>
2024-10-19 12:33:05 -0600

    ultimo

--- src/screens/FormularioRegistro.js
+++ src/screens/FormularioRegistro.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { View, TextInput, StyleSheet, Text, FlatList, Button, Alert } from 'react-native';
 import { Formik } from 'formik';
 import axios from 'axios';
-import CapturePhotos from './CapturePhotos'; // Asegúrate de importar el componente
+
 
 const FormularioRegistro = ({ route }) => {
   const { producto } = route.params;
 
   const handleSubmit = async (values) => {
     try {
+      const prestacionesJSON = JSON.stringify(producto);
       const response = await axios.post('http://192.168.1.12:3000/productos', {
         detalle: values.carnet,
         nombre_completo: `${values.nombres} ${values.apellidos}`,
@@ -16,6 +17,7 @@ const FormularioRegistro = ({ route }) => {
         foto_selfie: values.foto_selfie, // Aquí se usa la selfie en base64
         direccion: values.Direccion,
         telefono: values.Telefono,
+        prestaciones: prestacionesJSON 
       });
       Alert.alert('Éxito', 'Producto registrado correctamente');
     } catch (error) {
@@ -28,22 +30,21 @@ const FormularioRegistro = ({ route }) => {
       initialValues={{ nombres: '', apellidos: '', carnet: '', foto_carnet: '', foto_selfie: '', Direccion: '', Telefono: '' }}
       onSubmit={handleSubmit}
     >
-      {({ handleChange, handleSubmit, setFieldValue, values }) => (
+        {({ handleChange, handleSubmit, values }) => (
         <FlatList
           data={producto}
           keyExtractor={(item, index) => index.toString()}
           ListHeaderComponent={() => (
             <View style={styles.container}>
               <Text style={styles.title}>Formulario de Egresos</Text>
+              
               <TextInput placeholder="Nombres" onChangeText={handleChange('nombres')} value={values.nombres} style={styles.input} />
-              <TextInput placeholder="Apellidos" onChangeText={handleChange('apellidos')} value={values.apellidos} style={styles.input} />
-              <TextInput placeholder="Dirección" onChangeText={handleChange('Direccion')} value={values.Direccion} style={styles.input} />
-              <TextInput placeholder="Teléfono" onChangeText={handleChange('Telefono')} value={values.Telefono} style={styles.input} />
-
-              {/* Componente de captura de fotos */}
-              <CapturePhotos setFieldValue={setFieldValue} />
-
-              <Button title="Registrar Producto" onPress={handleSubmit} />
+                            <TextInput placeholder="Apellidos" onChangeText={handleChange('apellidos')} value={values.apellidos} style={styles.input} />
+                            <TextInput placeholder="Carnet" onChangeText={handleChange('carnet')} value={values.carnet} style={styles.input} />
+                            <TextInput placeholder="FotoPerfil (base64 o URL)" onChangeText={handleChange('FotoPerfil')} value={values.FotoPerfil} style={styles.input} />
+                            <TextInput placeholder="Dirección" onChangeText={handleChange('Direccion')} value={values.Direccion} style={styles.input} />
+                            <TextInput placeholder="Teléfono" onChangeText={handleChange('Telefono')} value={values.Telefono} style={styles.input} />
+                            <Button title="Registrar Producto" onPress={handleSubmit} />
             </View>
           )}
         />
